4885b4794ff1d6c26e4d52e06cd8a3d3190a8856
Paul Kelly <paul.kelly20@gmail.com>
2018-04-30 11:54:00 +0100

    Initial Commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner { // new class with name runner and its a public class
+
+    public static void main(String[] args){
+        Cat cat1 = new Cat();
+        System.out.println(cat1.meow()); // print line and then calling meow method from cat class
+
+    }
+}
